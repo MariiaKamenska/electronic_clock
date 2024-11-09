866a2df4892f4f75cc5d10ebfa1e754b812174f6
MariiaKamenska <kamenskaam53@gmail.com>
2024-11-09 20:05:17 +0200

    output timezone NewYork (-5)

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ function updateClock() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    
+
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
 setInterval(updateClock, 1000);
+
+function updateClock_timezone() {
+    const now_local = new Date();
+    const select_timezone = -5; //NewYork
+    const hours = String(now_local.getHours()-2+select_timezone).padStart(2, '0');
+    const minutes = String(now_local.getMinutes()).padStart(2, '0');
+    const seconds = String(now_local.getSeconds()).padStart(2, '0');
+    
+    document.getElementById('timezone').textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock_timezone, 1000);
