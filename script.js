b3440a2c9eaf7b44f514dfc0f60150e52ee2d74f
MariiaKamenska <kamenskaam53@gmail.com>
2024-11-16 20:15:26 +0200

    timezone_fixed

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function updateClock() {
+function updateClock(time) {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const hours = String(now.getUTCHours()+ time).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+    
+    timeZone = parseInt(selectTimeZone.value, 10);
+    // console.log(timeZone);
+
+    
 }
 
-setInterval(updateClock, 1000);
+
+
+let timeZone;
+
+const selectTimeZone = document.getElementById("timeZone");
+selectTimeZone.addEventListener('change', updateClock(timeZone));
+
+setInterval(updateClock(timeZone), 1000);
+updateClock(timeZone);
+
+// const different_time_UTC = new Date().getTimezoneOffset() / 60
+// const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+// document.getElementById('clock2').textContent = timezone;
