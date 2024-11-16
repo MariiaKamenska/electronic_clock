ea3c5228852e09d2c7299574b4ee788890e46cb8
MariiaKamenska <kamenskaam53@gmail.com>
2024-11-16 19:00:14 +0200

    print clock(our time) on site

--- script.js
+++ script.js
@@ -6,15 +6,5 @@ function updateClock() {
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
-setInterval(updateClock, 1000);
 
-function updateClock_timezone() {
-    const now_local = new Date();
-    const select_timezone = -5; //NewYork
-    const hours = String(now_local.getHours()-2+select_timezone).padStart(2, '0');
-    const minutes = String(now_local.getMinutes()).padStart(2, '0');
-    const seconds = String(now_local.getSeconds()).padStart(2, '0');
-    
-    document.getElementById('timezone').textContent = `${hours}:${minutes}:${seconds}`;
-}
-setInterval(updateClock_timezone, 1000);
+setInterval(updateClock, 1000);
