48a8a964e6230a0de84b1c6474e9ae2c0df6326b
MariiaKamenska <kamenskaam53@gmail.com>
2024-11-23 19:42:38 +0200

    enter utc time with select country

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-function updateClock(time) {
+const clockElement = document.getElementById('clock');
+const selectTimeZone = document.getElementById('timeZone');
+const local_timeZone = document.getElementById('local_timeZone');
+
+
+function updateClock() {
     const now = new Date();
-    const hours = String(now.getUTCHours()+ time).padStart(2, '0');
+    const timeZone = parseInt(selectTimeZone.value, 10);
+    const hours = String(now.getUTCHours()+ timeZone).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
     const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 
     
-    timeZone = parseInt(selectTimeZone.value, 10);
+
     // console.log(timeZone);
+    local_timeZone.textContent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     
 }
 
-
-
-let timeZone;
-
-const selectTimeZone = document.getElementById("timeZone");
-selectTimeZone.addEventListener('change', updateClock(timeZone));
-
-setInterval(updateClock(timeZone), 1000);
+setInterval(updateClock, 1000);
+selectTimeZone.addEventListener('change', updateClock);
 updateClock(timeZone);
 
 // const different_time_UTC = new Date().getTimezoneOffset() / 60
